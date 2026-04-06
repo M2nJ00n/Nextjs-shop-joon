1131aec1be3c8870ed20d79448975f7d287e031a
nmj <m2nj00n07@gmail.com>
2026-04-06 20:24:20 +0900

    fix - app\api\seed\route.ts 삭제 - 몇몇 안 나오는 상품 사진 나오게 하기

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'unsplash.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'res.cloudinary.com', // 추가
       },
     ],
